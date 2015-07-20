3bdc0141a270ec764dec019edf6c582dfc86b8d2
Pedro Lopes <pedrompflopes@gmail.com>
2015-07-20 17:11:16 +0100

    use indieshuffle api

--- bot/indieshuffle.py
+++ bot/indieshuffle.py
@@ -1,47 +1,32 @@
 import tgbot
 import requests
+import os
+import json
 from twx.botapi import ReplyKeyboardMarkup
-from lxml import html
 
 
-def _url(append):
-    return 'http://www.indieshuffle.com%s' % (append.replace(" ", "-"))
-
-
-def _send_songs(url, bot, message):
-    r = requests.get(url)
-    tree = html.fromstring(r.text)
-    songs = tree.xpath('//div[@class="songMidContent"]//following::td/div/a/@href')
-    for song in songs:
-        bot.tg.send_message(
-            message.chat.id,
-            _url(song),
-            reply_to_message_id=message.message_id)
+def _get_songs(service, count=1, page=1):
+    payload = {'key': os.environ['INDIESHUFFLE_KEY'], 'count': count, 'page': page}
+    r = requests.get(('http://www.indieshuffle.com/mobile/%s' % service), params=payload)
+    parsedjson = json.loads(r.text)
+    return parsedjson['posts']
 
 
 class IndieShuPlugin(tgbot.TGPluginBase):
     def __init__(self):
         super(IndieShuPlugin, self).__init__()
-        self._time_keyboard = [['Week'], ['Month']]
-        self._genre_keyboard = [['All'], ['Electronic'], ['Indie rock'], ['House'], ['Remixes'], ['Female Vocalist'], ['Ambient'], ['Folk']]
+        self._genre_keyboard = [['all'], ['electronic'], ['surf-rock'], ['indie-pop'], ['rac'], ['remale-vocalist'], ['covers']]
 
     def list_commands(self):
         return [
-            ('tsong', self.tsong, 'Song of the day!!!!!'),
+            ('tsong', self.tsong, 'Song of the day!'),
             ('latest', self.latest, 'Latests songs!'),
             ('popular', self.popular, 'Popular music!'),
-            ('popularthisweek', self.popularthisweek, 'Popular this week!'),
-            ('popularthismonth', self.popularthismonth, 'Popular this month!'),
         ]
 
     def tsong(self, bot, message, text):
-        r = requests.get(_url('/song-of-the-day'))
-        tree = html.fromstring(r.text)
-        song = tree.xpath('//a[starts-with(text(),"SONG OF THE DAY")]//following::td/div/a/@href')
-        if not song:
-            reply = 'not found :('
-        else:
-            reply = _url(song[0])
+        song = _get_songs('songsoftheday')
+        reply = song[0]['url']
         bot.tg.send_message(
             message.chat.id,
             reply,
@@ -63,20 +48,32 @@ class IndieShuPlugin(tgbot.TGPluginBase):
             self.latest_try, message, out_message=rep, selective=True)
 
     def latest_try(self, bot, message, text):
-        url = _url('')
-        if text != '' and text != 'All':
-            url = _url('/songs/' + text)
-        _send_songs(url, bot, message)
+        songs = _get_songs('', count=10)
+        for song in songs:
+            if (text == 'All'):
+                bot.tg.send_message(
+                    message.chat.id,
+                    song['url'],
+                    reply_to_message_id=message.message_id)
+                continue
+            for tag in song['tags']:
+                print tag['slug']
+                if text.lower() in tag['slug'].lower():
+                    bot.tg.send_message(
+                        message.chat.id,
+                        song['url'],
+                        reply_to_message_id=message.message_id)
+                    break
 
     def popular(self, bot, message, text):
         reply_markup = ReplyKeyboardMarkup.create(
-            keyboard=[['Week'], ['Month']],
+            keyboard=self._genre_keyboard,
             resize_keyboard=True,
             one_time_keyboard=True,
             selective=True)
         rep = bot.tg.send_message(
             message.chat.id,
-            'Please select the time: ',
+            'Please select the genre: ',
             reply_to_message_id=message.message_id,
             reply_markup=reply_markup
         ).wait()
@@ -84,55 +81,19 @@ class IndieShuPlugin(tgbot.TGPluginBase):
             self.popular_try, message, out_message=rep, selective=True)
 
     def popular_try(self, bot, message, text):
-        if text == 'Week':
-            self.popularthisweek(bot, message, text)
-            return
-        if text == 'Month':
-            self.popularthismonth(bot, message, text)
-            return
-        bot.tg.send_message(
-            message.chat.id,
-            ':(',
-            reply_to_message_id=message.message_id)
-
-    def popularthisweek(self, bot, message, text):
-        reply_markup = ReplyKeyboardMarkup.create(
-            keyboard=self._genre_keyboard,
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            selective=True)
-        rep = bot.tg.send_message(
-            message.chat.id,
-            'Please select the genre: ',
-            reply_to_message_id=message.message_id,
-            reply_markup=reply_markup
-        ).wait()
-        self.need_reply(
-            self.popularthisweek_try, message, out_message=rep, selective=True)
-
-    def popularthisweek_try(self, bot, message, text):
-        url = _url('/popular/week/')
-        if text != '' and text != 'All':
-            url = _url('/popular/week/' + text)
-        _send_songs(url, bot, message)
-
-    def popularthismonth(self, bot, message, text):
-        reply_markup = ReplyKeyboardMarkup.create(
-            keyboard=self._genre_keyboard,
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            selective=True)
-        rep = bot.tg.send_message(
-            message.chat.id,
-            'Please select the genre: ',
-            reply_to_message_id=message.message_id,
-            reply_markup=reply_markup
-        ).wait()
-        self.need_reply(
-            self.popularthismonth_try, message, out_message=rep, selective=True)
-
-    def popularthismonth_try(self, bot, message, text):
-        url = _url('/popular/month/')
-        if text != '' and text != 'All':
-            url = _url('/popular/month/' + text)
-        _send_songs(url, bot, message)
+        songs = _get_songs('track/popular/', count=10)
+        for song in songs:
+            if (text == 'All'):
+                bot.tg.send_message(
+                    message.chat.id,
+                    song['url'],
+                    reply_to_message_id=message.message_id)
+                continue
+            for tag in song['tags']:
+                print tag['slug']
+                if text.lower() in tag['slug'].lower():
+                    bot.tg.send_message(
+                        message.chat.id,
+                        song['url'],
+                        reply_to_message_id=message.message_id)
+                    break
--- tests.py
+++ tests.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import unittest
+from bot.indieshuffle import IndieShuPlugin
 
-# TODO
+class IndieShuffleBotTest(unittest.TestCase):
+
+    def test_songofday(self):
+        isb = IndieShuPlugin()
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
